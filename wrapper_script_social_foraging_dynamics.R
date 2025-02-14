db5c4706ae413530bd68f3769f0cac711e9d0669
Alexander Schakowski <131854460+aschakowski@users.noreply.github.com>
2025-02-14 10:11:43 +0100

    Update wrapper_script_social_foraging_dynamics.R

--- wrapper_script_social_foraging_dynamics.R
+++ wrapper_script_social_foraging_dynamics.R
@@ -41,7 +41,7 @@ source("1_icefishing_social_foraging/wrapper_script_icefishing_social_foraging.R
 
 # This function runs the analyses and creates output. To reproduce analyses,
 # script can be run on its own.
-run_1_icefishing_social_foraging(wd)
+run_1_icefishing_social_foraging()
 
 ################################################################################
 # 2. Socio-ecological features drive adaptive spatial search
@@ -63,7 +63,7 @@ run_2_3_socio_ecological_features()
 # The script runs depletion models and patch--leaving model.
 # To see intermediary results, run script stand-alone.
 source("4_incremental_strategies/wrapper_script_incremental_strategies.R")
-run_4_incremental_strategies(wd)
+run_4_incremental_strategies()
 
 ################################################################################
 # 5. Inter--individual differences in foraging strategies
@@ -72,7 +72,7 @@ run_4_incremental_strategies(wd)
 # The scripts investigates inter--individual differences and produces figures.
 # To see results, run script on its own.
 source("5_inter_individual_differences/wrapper_script_inter_ind_differences.R")
-run_5_inter_individual_differences(wd)
+run_5_inter_individual_differences()
 
 ################################################################################
 # END
