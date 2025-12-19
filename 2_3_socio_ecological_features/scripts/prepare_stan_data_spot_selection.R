238203be5a4288ab2a4d2c4df258b4ccaf02a486
Alexander Schakowski <131854460+aschakowski@users.noreply.github.com>
2025-12-19 18:35:08 +0100

    Add files via upload

---
+++ 2_3_socio_ecological_features/scripts/prepare_stan_data_spot_selection.R
@@ -0,0 +1,375 @@
+################################################################################
+#
+# Title: Prepare Stan Data
+#
+# Description: Prepare stan data for model fit
+#
+# Authors: Schakowski, A.
+#
+# Last updated: 13/02/2024 (DD/MM/YYYY)
+#
+################################################################################
+prepare_stan_data_spot_selection <- function(){
+  
+  ################################################################################
+  # Helper Functions
+  ################################################################################
+  # in stan, all matrices in array need to have same dimensions, thus some matrices
+  # need to be filled.
+  pad_matrix <- function(matrix, new_rows, new_columns, filler){
+    
+    if ( new_rows > nrow(matrix) & new_columns > ncol(matrix)){
+      
+      # add columns
+      column_diff = new_columns - ncol(matrix)
+      columns = replicate(n = column_diff, expr = rep(filler, nrow(matrix)))
+      matrix = cbind(matrix, columns)
+      
+      # add rows
+      row_diff = new_rows - nrow(matrix)
+      rows = t(replicate(n = row_diff, expr = rep(filler, ncol(matrix))))
+      matrix = rbind(matrix, rows)
+      
+    } else if (new_rows > nrow(matrix) & new_columns == ncol(matrix)){
+      
+      # add rows
+      row_diff = new_rows - nrow(matrix)
+      rows = t(replicate(n = row_diff, expr = rep(filler, ncol(matrix))))
+      matrix = rbind(matrix, rows)
+      
+    } else if (new_rows == nrow(matrix) & new_columns > ncol(matrix) & new_rows > 1){
+      
+      # add columns
+      column_diff = new_columns - ncol(matrix)
+      columns = replicate(n = column_diff, expr = rep(filler, nrow(matrix)))
+      matrix = cbind(matrix, columns)
+      
+    } else if (new_rows == 1){
+      
+      ## add columns
+      column_diff = new_columns - ncol(matrix)
+      columns = replicate(n = column_diff, expr = rep(filler, 1))
+      matrix = c(matrix, columns)
+      
+    } else {
+      # do nothing
+      #print("New dimensions are old dimensions.")
+    }
+    
+    return(matrix)
+  }
+  
+  prepare_stan_data <- function(social_distances,
+                                roughness_distances,
+                                ecological_features,
+                                success_distances, 
+                                loss_distances, 
+                                locality,
+                                time_since_start,
+                                grainsize, 
+                                identifiers, 
+                                ncolumns,
+                                option = "full",
+                                missing_ids = 0){
+    
+    # subset ids
+    if (option == "id_level"){
+      sub_ids = identifiers[identifiers$spot_id != 1 & !identifiers$participant_id %in% missing_ids, ]
+    } else {
+      sub_ids = identifiers[identifiers$spot_id != 1, ]
+    }
+    
+    # lake index
+    sub_ids$lake_index = as.numeric(as.factor(sub_ids$lake_id))
+    lake = sub_ids$lake_index
+    N_lakes = length(unique(sub_ids$lake_index))
+    
+    # trip index
+    sub_ids$trip_index = as.numeric(as.factor(sub_ids$track_id))
+    trip = sub_ids$trip_index
+    N_trips = length(unique(sub_ids$trip_index))
+    
+    # participant index
+    sub_ids$id_index = as.numeric(as.factor(sub_ids$participant_id))
+    id = sub_ids$id_index
+    N_ids = length(unique(sub_ids$id_index))
+    
+    # join back (keep for later matching participant ids/lake ids back to model ests)
+    identifiers = left_join(identifiers, sub_ids)
+    if (option == "id_level"){
+      saveRDS(identifiers, file = "utils/data/processed_data/identifiers_model_key_grp_level_predictors.rds")
+    } else {
+      saveRDS(identifiers, file = "utils/data/processed_data/identifiers_model_key.rds")
+    }
+    
+    # id level predictors
+    id_level_predictors = sub_ids %>% 
+      group_by(id_index) %>% 
+      slice(1) %>% 
+      arrange(id_index) %>% 
+      dplyr::select(age, sex, angling_skill)
+    id_level_predictors = as.data.frame(id_level_predictors)[2:4]
+    
+    # lake level predictors
+    lake_level_predictors = sub_ids %>% 
+      group_by(lake_index) %>% 
+      slice(1) %>% 
+      arrange(lake_index) %>% 
+      dplyr::select(catch, mean_return)
+    lake_level_predictors = as.data.frame(lake_level_predictors)[2:3]
+    
+    # keep key to link new ids to old ones
+    key_participant_id = data.frame(participant_id = sub_ids$participant_id,
+                                    model_participant_id = id)
+    key_track_id = data.frame(track_id = sub_ids$track_id,
+                              model_track_id = trip)
+    key_lake_id = data.frame(lake_id = sub_ids$lake_id,
+                             model_lake_id = lake)
+    keys = list(key_participant_id,
+                key_track_id,
+                key_lake_id)
+    
+    if (option == "id_level"){
+      saveRDS(key, file = "utils/data/processed_data/model_key_grp_level_predictors.rds")
+    } else {
+      saveRDS(key, file = "utils/data/processed_data/model_key.rds")
+    }
+    
+    
+    # compute success columns (how many columns need to be subset within model)
+    success_columns = vector(mode = "numeric", length = length(success_distances))
+    for (i in 1:length(success_distances)){
+      
+      if (is.null(dim(success_distances[[i]]))){
+        success_columns[i] <- 1
+      } else {
+        success_columns[i] <- ncol(success_distances[[i]])
+        success_columns[i] <- min(ncolumns, success_columns[[i]]) 
+      }
+    }
+    
+    # compute loss columns
+    loss_columns = vector(mode = "numeric", length = length(loss_distances))
+    for (i in 1:length(loss_distances)){
+      
+      if (is.null(dim(loss_distances[[i]]))){
+        loss_columns[i] <- 1
+      } else {
+        loss_columns[i] <- ncol(loss_distances[[i]])
+        loss_columns[i] <- min(ncolumns, loss_columns[[i]]) 
+      }
+    }
+    
+    # select closest ncol spots/ids
+    for (i in 1:length(social_distances)){
+      
+      # social
+      social_distances[[i]][is.na(social_distances[[i]])] <- 0.0001 #missings happen rarely and bc. of low dist (<.001%)
+      social_distances[[i]] = t(apply(social_distances[[i]], 1, sort))
+      social_distances[[i]] <- social_distances[[i]][, 1:ncolumns]
+      
+      # roughness
+      roughness_distances[[i]][is.na(roughness_distances[[i]])] <- 0.0001
+      roughness_distances[[i]] = t(apply(roughness_distances[[i]], 1, sort))
+      roughness_distances[[i]] <- as.matrix(roughness_distances[[i]])[, 1:ncolumns]
+      
+      # select last ncol spots
+      if (success_columns[[i]] == 1){
+        success_distances[[i]] = success_distances[[i]]
+      } else {
+        success_distances[[i]] = success_distances[[i]][, 1:success_columns[i]]
+      }
+      
+      if (loss_columns[[i]] == 1){
+        loss_distances[[i]] = loss_distances[[i]]
+      } else {
+        loss_distances[[i]] = loss_distances[[i]][, 1:loss_columns[i]] 
+      }
+      
+    }
+    
+    # pad matrix to max columns
+    for (i in 1:length(social_distances)){
+      
+      social_distances[[i]] <- pad_matrix(social_distances[[i]],
+                                          new_rows = nrow(social_distances[[i]]),
+                                          new_columns = ncolumns,
+                                          filler = -99)
+      
+      success_distances[[i]] <- pad_matrix(as.matrix(success_distances[[i]]),
+                                           new_rows = nrow(as.matrix(success_distances[[i]])),
+                                           new_columns = ncolumns,
+                                           filler = -99)
+      
+      roughness_distances[[i]] <- pad_matrix(roughness_distances[[i]],
+                                             new_rows = nrow(roughness_distances[[i]]),
+                                             new_columns = ncolumns,
+                                             filler = -99)
+      
+      loss_distances[[i]] <- pad_matrix(as.matrix(loss_distances[[i]]),
+                                        new_rows = nrow(as.matrix(loss_distances[[i]])),
+                                        new_columns = ncolumns,
+                                        filler = -99)
+      
+    }
+    
+    # convert list of matrices to stan conform array
+    social_feature <- abind(social_distances, along=3)
+    social_feature <- aperm(social_feature, c(3,1,2)) #dim 1: time, dim2: alternative spots, dim3: distances to 55 players
+    
+    success_feature <- abind(success_distances, along=3)
+    success_feature <- aperm(success_feature, c(3,1,2)) #dim 1: time, dim2: alternative spots, dim3: distances to success spots
+    
+    loss_feature <- abind(loss_distances, along=3)
+    loss_feature <- aperm(loss_feature, c(3,1,2)) #dim 1: time, dim2: alternative spots, dim3: distances to unsuccessful spots
+    
+    roughness_feature <- abind(roughness_distances, along=3)
+    roughness_feature <- aperm(roughness_feature, c(3,1,2)) #dim 1: time, dim2: alternative spots, dim3: distances to depth curves
+    
+    ecological_features <- abind(ecological_features, along = 3)
+    ecological_features <- aperm(ecological_features, c(3, 1, 2))
+    
+    # locality matrix: columns: all spot choices, rows: alternative spots
+    locality_feature = do.call(cbind, locality)
+    
+    # number of alternative spots
+    N_spots = nrow(social_distances[[1]])
+    
+    # choice vector
+    choices = 1:length(social_distances)
+    
+    # grainsize for within chain parallelization
+    grainsize = grainsize
+    
+    # return padded distance matrix and columns
+    if (option == "id_level"){
+      return(list(Distances_social = social_feature,
+                  Distances_roughness = roughness_feature, 
+                  Ecological_features = ecological_features,
+                  Distances_successes = success_feature,
+                  Distances_losses = loss_feature,
+                  Locality = locality_feature,
+                  Time_since_start = as.numeric(time_since_start)/max(as.numeric(time_since_start)),
+                  success_columns = success_columns, 
+                  loss_columns = loss_columns,
+                  T = length(social_distances),
+                  N_spots = N_spots, 
+                  choices = choices, 
+                  grainsize = grainsize,
+                  columns = ncolumns,
+                  lake = lake,
+                  N_lakes = N_lakes,
+                  trip = trip,
+                  N_trips = N_trips, 
+                  id = id,
+                  N_ids = N_ids,
+                  id_level_predictors = id_level_predictors,
+                  lake_level_predictors = lake_level_predictors)) 
+    } else {
+      return(list(Distances_social = social_feature,
+                  Distances_roughness = roughness_feature, 
+                  Ecological_features = ecological_features,
+                  Distances_successes = success_feature,
+                  Distances_losses = loss_feature,
+                  Locality = locality_feature,
+                  Time_since_start = as.numeric(time_since_start)/max(as.numeric(time_since_start)),
+                  success_columns = success_columns, 
+                  loss_columns = loss_columns,
+                  T = length(social_distances),
+                  N_spots = N_spots, 
+                  choices = choices, 
+                  grainsize = grainsize,
+                  columns = ncolumns,
+                  lake = lake,
+                  N_lakes = N_lakes,
+                  trip = trip,
+                  N_trips = N_trips, 
+                  id = id,
+                  N_ids = N_ids))
+    }
+    
+  }
+  ################################################################################
+  # spot ids
+  identifiers = readRDS(file = "utils/data/processed_data/identifiers_spot_selection.rds")
+  
+  # join id level predictors to identifiers
+  catch_data <- read_csv("utils/data/raw_data/catch_data.csv")
+  variables = catch_data %>% 
+    group_by(participant_id) %>% 
+    slice(1) %>% 
+    dplyr::select(sex, year_of_birth)
+  variables$age = 2023-variables$year_of_birth
+  variables$sex = 2-as.numeric(as.factor(variables$sex))
+  variables$participant_id = as.character(variables$participant_id)
+  identifiers = left_join(identifiers, variables)
+  
+  survey_data = read_csv("utils/data/raw_data/survey_data.csv")
+  skill = survey_data %>% dplyr::select(participant_id, angling_skill)
+  skill$participant_id = as.character(skill$participant_id)
+  identifiers = left_join(identifiers, skill)
+  
+  # join lake level predictors
+  lake_variables = catch_data %>% group_by(day, year) %>% summarize(catch = mean(catch))
+  ret = spot_selection_data %>% group_by(day, year) %>% summarize(mean_return = mean(Return))
+  lake_variables = left_join(lake_variables, ret)
+  lake_variables$day = as.character(lake_variables$day)
+  lake_variables$year = as.character(lake_variables$year)
+  identifiers = left_join(identifiers, lake_variables)
+  
+  # load raw features
+  social_distances = readRDS(file = "utils/data/processed_data/social_distance_matrix.rds")
+  roughness_distances = readRDS(file = "utils/data/processed_data/roughness_distance_matrix.rds")
+  ecological_features = readRDS(file = "utils/data/processed_data/alternative_roughness_distance_matrix.rds")
+  success_distances = readRDS(file = "utils/data/processed_data/success_distance_matrix.rds")
+  loss_distances = readRDS(file = "utils/data/processed_data/loss_distance_matrix.rds")
+  locality = readRDS(file = "utils/data/processed_data/locality_matrix.rds")
+  
+  # time since start as feature
+  time_since_start = identifiers$time_since_start
+  
+  # drop first choices
+  social_distances = social_distances[(identifiers$spot_id!= 1)]
+  roughness_distances = roughness_distances[(identifiers$spot_id!= 1)]
+  ecological_features = ecological_features[(identifiers$spot_id!= 1)]
+  success_distances = success_distances[(identifiers$spot_id!= 1)]
+  loss_distances = loss_distances[(identifiers$spot_id!= 1)]
+  locality = locality[(identifiers$spot_id!= 1)]
+  time_since_start = time_since_start[identifiers$spot_id!=1]
+  
+  # compute feature matrix
+  data_list = prepare_stan_data(social_distances = social_distances,
+                                roughness_distances = roughness_distances, 
+                                ecological_features = ecological_features,
+                                success_distances = success_distances,
+                                loss_distances = loss_distances,
+                                locality = locality, 
+                                time_since_start = time_since_start,
+                                grainsize = 1, identifiers = identifiers, ncolumns = 15)
+  saveRDS(data_list, file = "utils/data/processed_data/stan_data_spot_selection.rds")
+  
+  ################################################################################
+  # REVISION: Create stan data for id level predictions (remove missings first)
+  ################################################################################
+  # note. only used for suppl. mat.
+  # which ids are missing?
+  missing_ids = unique(identifiers$participant_id[which(is.na(identifiers$angling_skill))])
+  
+  # compute feature matrix
+  data_list = prepare_stan_data(social_distances = social_distances,
+                                roughness_distances = roughness_distances, 
+                                ecological_features = ecological_features,
+                                success_distances = success_distances,
+                                loss_distances = loss_distances,
+                                locality = locality, 
+                                time_since_start = time_since_start,
+                                grainsize = 1, identifiers = identifiers, ncolumns = 15,
+                                option = "id_level",
+                                missing_ids = missing_ids)
+  saveRDS(data_list, file = "utils/data/processed_data/stan_data_spot_selection_grp_level_predictors.rds")
+  
+}
+
+################################################################################
+# END
+################################################################################
---
+++ 2_3_socio_ecological_features/scripts/run_model_comparison_spot_selection.R
@@ -0,0 +1,218 @@
+################################################################################
+#
+# Title: Run and compare spot selection models
+#
+# Authors: Schakowski, A.
+#
+# Last updated: 27/07/2024 (DD/MM/YYYY)
+#
+################################################################################
+run_model_comparison_spot_selection <- function(){
+  
+################################################################################
+# helper function
+
+# fit models
+model_fit <- function(model_location, save_location, data_list, warmup, iterations, chains, fixed_param){
+  # load model
+  stan_model = cmdstan_model(model_location, 
+                             cpp_options = list(stan_threads = TRUE))
+  
+  # fit models
+  stan_model_fit <- stan_model$sample(
+    data = data_list,
+    chains = chains,
+    parallel_chains = chains,
+    refresh = 5,
+    iter_sampling = iterations,
+    iter_warmup = warmup,
+    threads_per_chain = round(40/chains),
+    fixed_param = fixed_param
+  )
+  
+  # save
+  stan_model_fit$save_object(file = save_location)
+  
+  # empty return
+  return()
+  
+}
+
+# extract log lik
+compute_waic <- function(fit_location, save_location){
+  
+  # load model fit
+  model_fit = readRDS(file = fit_location)
+  
+  # compute waic
+  ll = model_fit$draws(variables = "log_lik")
+  waic = waic(ll)
+  
+  # save waic
+  saveRDS(waic, file = save_location)
+  
+  # keep workspace clear
+  gc()
+  
+  # empty return
+  return()
+  
+}
+
+################################################################################
+# load stan data
+stan_data = readRDS(file = "utils/data/processed_data/stan_data_spot_selection.rds")
+
+# standardized time since start from 0 to 1
+stan_data$Time_since_start = as.numeric(stan_data$Time_since_start)
+
+# any 0s in dataset? If so, replace with small value
+stan_data$Distances_social[stan_data$Distances_social == 0] <- .001
+stan_data$Distances_roughness[stan_data$Distances_roughness == 0] <- .001
+stan_data$Distances_success[stan_data$Distances_success == 0] <- .001
+stan_data$Distances_loss[stan_data$Distances_loss == 0] <- .001
+stan_data$Locality[stan_data$Locality == 0] <- .001
+
+# any missings in dataset? (n = 0)
+stan_data$Distances_social[is.na(stan_data$Distances_social)]
+stan_data$Distances_roughness[is.na(stan_data$Distances_roughness)]
+stan_data$Distances_success[is.na(stan_data$Distances_success)]
+stan_data$Distances_loss[is.na(stan_data$Distances_loss)]
+stan_data$Locality[is.na(stan_data$Locality)]
+
+# fit models
+model_locations = c("utils/stan/spot_selection_models/a_random_selection.stan",
+                    "utils/stan/spot_selection_models/b_no_roughness.stan",
+                    "utils/stan/spot_selection_models/c_no_personal.stan",
+                    "utils/stan/spot_selection_models/d_no_social.stan",
+                    "utils/stan/spot_selection_models/e_all_features.stan",
+                    "utils/stan/spot_selection_models/f_conditional.stan",
+                    "utils/stan/spot_selection_models/g_alternative_roughness_model.stan",
+                    "utils/stan/spot_selection_models/g_alternative_roughness_model.stan",
+                    "utils/stan/spot_selection_models/g_alternative_roughness_model.stan",
+                    "utils/stan/spot_selection_models/h_roughness_depth_model.stan")
+save_locations = c("utils/data/processed_data/model_fit/spot_selection_models/a_random_selection.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/b_no_roughness.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/c_no_personal.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/d_no_social.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/e_all_features.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/f_conditional.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/g_alternative_roughness_variance.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/g_alternative_roughness_min_max.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/g_alternative_roughness_mean_dist.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/h_roughness_depth_fit.rds")
+
+# note: exact numerical parameter estimates not reproducible, because of randomness in simulation 
+# of alternative spots. Model was fit multiple times to ensure that the direction, size and credibility
+# of effects is not affected by this randomness.
+for (i in 1:length(model_locations)){
+  
+  if (i == 1){
+    
+    # fit random model using fixed params.
+    model_fit(model_location = model_locations[i],
+              save_location = save_locations[i],
+              data_list = stan_data,
+              warmup = 1000,
+              iterations = 2000,
+              chains = 4,
+              fixed_param = T)
+  } else {
+    
+    # subset correct features for alternative eco feature models (suppl. text.)
+    if (i == 6){
+      stan_data$feature_index = 1
+    } else if (i == 7){
+      stan_data$feature_index = 2
+    } else if (i == 8){
+      stan_data$feature_index = 3
+    }
+    
+    model_fit(model_location = model_locations[i],
+              save_location = save_locations[i],
+              data_list = stan_data,
+              warmup = 1000,
+              iterations = 2000,
+              chains = 4,
+              fixed_param = F)
+    
+  }
+  
+}
+
+# run model comparison
+fit_locations = save_locations
+save_locations = c("utils/data/processed_data/model_fit/spot_selection_models/comparison/a_random_selection.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/b_no_roughness.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/c_no_personal.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/d_no_social.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/e_all_features.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/f_conditional.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/g_alternative_roughness_variance.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/g_alternative_roughness_min_max.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/g_alternative_roughness_mean_dist.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/h_roughness_depth_fit.rds")
+
+for (i in 1:length(fit_locations)){
+  compute_waic(fit_location = fit_locations[i],
+               save_location = save_locations[i])
+}
+
+# conduct model comparison
+loo_list = list(readRDS(save_locations[1]), 
+                readRDS(save_locations[2]), 
+                readRDS(save_locations[3]), 
+                readRDS(save_locations[4]), 
+                readRDS(save_locations[5]),
+                readRDS(save_locations[6]))
+comparison = loo_compare(loo_list)
+
+# summarize for table
+#save_locations = c("utils/data/processed_data/model_fit/spot_selection_models/a_random_selection.rds",
+#                   "utils/data/processed_data/model_fit/spot_selection_models/b_no_roughness.rds",
+#                   "utils/data/processed_data/model_fit/spot_selection_models/c_no_personal.rds",
+#                   "utils/data/processed_data/model_fit/spot_selection_models/d_no_social.rds",
+#                   "utils/data/processed_data/model_fit/spot_selection_models/e_all_features.rds",
+#                   "utils/data/processed_data/model_fit/spot_selection_models/f_conditional.rds")
+
+# convergence diagnostics
+#max_rhats = vector()
+#for (i in 1:5){
+#  model = readRDS(save_locations[i+1])
+#  draws_random = model$draws(format = "df")
+#  rhats = model$summary()
+#  rhats = rhats[!str_detect(rhats$variable, "log_lik"), "rhat"]
+#}
+
+# save results
+saveRDS(comparison, file = "utils/data/processed_data/model_fit/spot_selection_models/comparison/comparison.rds")
+
+# run model comparison for alternative roughness features
+fit_locations = c("utils/data/processed_data/model_fit/spot_selection_models/g_alternative_roughness_variance.rds",
+                  "utils/data/processed_data/model_fit/spot_selection_models/g_alternative_roughness_min_max.rds",
+                  "utils/data/processed_data/model_fit/spot_selection_models/g_alternative_roughness_mean_dist.rds",
+                  "utils/data/processed_data/model_fit/spot_selection_models/h_roughness_depth_fit.rds")
+save_locations = c("utils/data/processed_data/model_fit/spot_selection_models/comparison/g_alternative_roughness_variance.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/g_alternative_roughness_min_max.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/g_alternative_roughness_mean_dist.rds",
+                   "utils/data/processed_data/model_fit/spot_selection_models/comparison/h_roughness_depth_fit.rds")
+
+for (i in 1:length(fit_locations)){
+  compute_waic(fit_location = fit_locations[i],
+               save_location = save_locations[i])
+}
+
+# conduct model comparison
+loo_list = list(readRDS("utils/data/processed_data/model_fit/spot_selection_models/comparison/c_no_personal.rds"), 
+                readRDS(save_locations[1]), 
+                readRDS(save_locations[2]), 
+                readRDS(save_locations[3]), 
+                readRDS(save_locations[4]))
+comparison = loo_compare(loo_list)
+
+# save results
+saveRDS(comparison, file = "utils/data/processed_data/model_fit/spot_selection_models/comparison/comparison_alternative_roughness.rds")
+}
+################################################################################
+# END
+################################################################################
